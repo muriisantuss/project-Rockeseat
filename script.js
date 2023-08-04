7befdddced10d3aa4bab7edcb519713009ec2c9f
Murilo Santos <muriisantuss@gmail.com>
2023-08-04 12:23:16 -0300

    uptade

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function toggleMode(){
     if(html.classList.contains('light')){
       img.setAttribute('src', 'assets/avatar-light.png')
     }else{
-      img.setAttribute("src", "assets/avatar.png")
+      img.setAttribute("src", "assets/Avatar.png")
     }
 
   
